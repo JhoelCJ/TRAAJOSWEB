f67ba5a71b48a9a4043b7ff6c58fd232e0e68685
Calderon Calva Jilmar Jhoel <jhoeljcalderon02@gmail.com>
2026-05-02 21:14:38 -0500

    verificar

--- hw06ORMandPHP/connect.php
+++ hw06ORMandPHP/connect.php
@@ -1,11 +1,18 @@
 <?php
 require_once 'rb.php';
 
-$host = getenv('DB_HOST');
-$db   = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$port = getenv('DB_PORT');
+$host = getenv('DB_HOST') ?: 'db.kljlrchsawiteqearbgy.supabase.co';
+$db   = getenv('DB_NAME') ?: 'postgres';
+$user = getenv('DB_USER') ?: 'postgres';
+$pass = getenv('DB_PASS') ?: 'WebAvWorksProject';
+$port = getenv('DB_PORT') ?: '5432';
+
+try {
+    $testConnection = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+} catch (PDOException $e) {
+    header('Content-Type: application/json');
+    die(json_encode(['status' => 'error', 'message' => 'Detalle exacto del error: ' . $e->getMessage()]));
+}
 
 try {
     if (!R::testConnection()) {
@@ -13,6 +20,6 @@ try {
     }
 } catch (Exception $e) {
     header('Content-Type: application/json');
-    die(json_encode(['status' => 'error', 'message' => 'Database connection failed.']));
+    die(json_encode(['status' => 'error', 'message' => 'Error de ORM: ' . $e->getMessage()]));
 }
 ?>
